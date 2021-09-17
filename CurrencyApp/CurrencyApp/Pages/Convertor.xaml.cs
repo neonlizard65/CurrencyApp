fcbe1ce2ba8b45e8dc2dcb0f681073ce84da0126
Alex Lapchik <alexlapchik@mail.ru>
2021-09-17 19:25:28 +0300

    Функционал Конвертера готов

--- CurrencyApp/CurrencyApp/Pages/Convertor.xaml.cs
+++ CurrencyApp/CurrencyApp/Pages/Convertor.xaml.cs
@@ -6,15 +6,69 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Data;
+using ServiceReference1;
+using System.Xml.Linq;
+using System.IO;
+using static CurrencyApp.CurrentCurrencyClass;
 
 namespace CurrencyApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Convertor : ContentPage
     {
+        List<ValuteDataValuteCursOnDate> AllValutes = new List<ValuteDataValuteCursOnDate>();
         public Convertor()
         {
             InitializeComponent();
+
+            //Подклчение и загрузка данных их ЦБ
+            DailyInfoSoapClient client = new DailyInfoSoapClient(DailyInfoSoapClient.EndpointConfiguration.DailyInfoSoap);
+            var curstoday = client.GetCursOnDate(DateTime.Now);
+            DataTable dt = XElementToDataTable(curstoday.Nodes[0]);
+
+            List<DataRow> drlist = new List<DataRow>();
+            foreach(DataRow dr in dt.Rows)
+            {
+                drlist.Add(dr);
+            }
+
+            
+            foreach (DataRow x in dt.Rows)
+            {
+                drlist.Add(x);
+                AllValutes.Add(new ValuteDataValuteCursOnDate(
+                    x[0].ToString(),
+                    ushort.Parse(x[1].ToString()),
+                    decimal.Parse(x[2].ToString()),
+                    x[4].ToString())
+                    );
+            }
+            CurrencyPicker1.ItemsSource = AllValutes;
+            CurrencyPicker2.ItemsSource = AllValutes;
+
         }
-    }
+
+        public DataTable XElementToDataTable(XElement element)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(new StringReader(element.ToString()));
+            return ds.Tables[0];
+        }
+
+        private void Convert_Btn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal firstVal = Convert.ToDecimal(Valute1.Text);
+                ValuteDataValuteCursOnDate val1 = (ValuteDataValuteCursOnDate)CurrencyPicker1.SelectedItem;
+                ValuteDataValuteCursOnDate val2 = (ValuteDataValuteCursOnDate)CurrencyPicker2.SelectedItem;
+                Valute2.Text = ((val1.Vcurs / val2.Vcurs) * firstVal).ToString("F4");
+            }
+            catch
+            {
+
+            }
+        }
+    }    
 }
