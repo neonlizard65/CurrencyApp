4f427d0b2d2f5a22b4ec5ef142e47f0ae570ad19
Alex Lapchik <alexlapchik@mail.ru>
2021-09-20 00:18:47 +0300

    Release 1.0.0

--- CurrencyApp/CurrencyApp.Android/MainActivity.cs
+++ CurrencyApp/CurrencyApp.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Xamarin.Essentials;
 
 namespace CurrencyApp.Droid
 {
-    [Activity(Label = "CurrencyApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ЦБР", Icon = "@drawable/bank", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
